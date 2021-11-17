e890c0f375f967522d7ff4ff4117cb3153908be2
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-17 23:08:46 +0100

    Inproved the bootloader

--- boot.asm
+++ boot.asm
@@ -1,7 +1,16 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+jmp 0x7c0:start
 start:
+  cli ; Clear Interrupts
+  mov ax, 0x7c0
+  mov ds, ax
+  mov es, ax
+  mov ax, 0x00
+  mov ss, ax
+  mov sp, 0x7c00
+  sti ; Enables Interrupts
   mov si, message
   call print
   jmp $
