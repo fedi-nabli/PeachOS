ff82f2167d9c108acce0a6ce95a1f6dc27b82a38
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-18 00:02:53 +0100

    Added Makefile and read from disk

--- boot.asm
+++ boot.asm
@@ -10,20 +10,6 @@ times 33 db 0
 start:
   jmp 0x7c0:step2
 
-handle_zero:
-  mov ah, 0eh
-  mov al, 'A'
-  mov bx, 0x00
-  int 0x10
-  iret
-
-handle_one:
-  mov ah, 0eh
-  mov al, 'V'
-  mov bx, 0x00
-  int 0x10
-  iret
-
 step2:
   cli ; Clear Interrupts
   mov ax, 0x7c0
@@ -34,15 +20,20 @@ step2:
   mov sp, 0x7c00
   sti ; Enables Interrupts
 
-  mov word[ss:0x00], handle_zero
-  mov word[ss:0x02], 0x7c0
+  mov ah, 2 ; READ SECTOR COMMAND
+  mov al, 1 ; ONE SECTOR TO READ
+  mov ch, 0 ;  Cylinder low eight bits
+  mov cl, 2 ; Read sector two
+  mox bx, buffer
+  int 0x13
+  jc error
 
-  mov word[ss:0x04], handle_one
-  mov word[ss:0x06], 0x7c0
+  mov si, buffer
+  call print
+  jmp $
 
-  int 1
-
-  mov si, message
+error:
+  mov si, error_message
   call print
   jmp $
 
@@ -64,7 +55,9 @@ print_char:
   int 0x10
   ret
 
-message: db 'Hello worls!', 0
+error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
