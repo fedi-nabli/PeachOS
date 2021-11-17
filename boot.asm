89735a82bc0fac37271463c00b30cbc361211d0f
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-17 23:35:44 +0100

    Implementing interrupts

--- boot.asm
+++ boot.asm
@@ -10,6 +10,20 @@ times 33 db 0
 start:
   jmp 0x7c0:step2
 
+handle_zero:
+  mov ah, 0eh
+  mov al, 'A'
+  mov bx, 0x00
+  int 0x10
+  iret
+
+handle_one:
+  mov ah, 0eh
+  mov al, 'V'
+  mov bx, 0x00
+  int 0x10
+  iret
+
 step2:
   cli ; Clear Interrupts
   mov ax, 0x7c0
@@ -19,6 +33,15 @@ step2:
   mov ss, ax
   mov sp, 0x7c00
   sti ; Enables Interrupts
+
+  mov word[ss:0x00], handle_zero
+  mov word[ss:0x02], 0x7c0
+
+  mov word[ss:0x04], handle_one
+  mov word[ss:0x06], 0x7c0
+
+  int 1
+
   mov si, message
   call print
   jmp $
