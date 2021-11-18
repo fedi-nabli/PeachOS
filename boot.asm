3e2e4a0ca7b7088e9d08fb1405bbe91862bf4c20
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-18 17:47:11 +0100

    Switched to protected mode

--- boot.asm
+++ boot.asm
@@ -1,6 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
 
+CODE_SEG equ
+DATA_SEG equ
 _start:
   jmp short start
   nop
@@ -8,56 +10,66 @@ _start:
 times 33 db 0
 
 start:
-  jmp 0x7c0:step2
+  jmp 0:step2
 
 step2:
   cli ; Clear Interrupts
-  mov ax, 0x7c0
+  mov ax, 0x00
   mov ds, ax
   mov es, ax
-  mov ax, 0x00
   mov ss, ax
   mov sp, 0x7c00
   sti ; Enables Interrupts
 
-  mov ah, 2 ; READ SECTOR COMMAND
-  mov al, 1 ; ONE SECTOR TO READ
-  mov ch, 0 ;  Cylinder low eight bits
-  mov cl, 2 ; Read sector two
-  mox bx, buffer
-  int 0x13
-  jc error
+.load_protected:
+  cli
+  lgdt[gdt_descriptor]
+  mov eax, cr0
+  or eax, 0x1
+  lov cr0, eax
+  jmp CODE_SEG:load32
 
-  mov si, buffer
-  call print
+; GDT
+gdt_start:
+gdt_null:
+  dd 0x0
+  dd 0x0
+
+; offset 0x0
+gdt_code:   ; CS SHOULD POINT TO THIS
+  dw 0xffff ; Segment limit first 0-15 bits
+  dw 0      ; Base first 0-15 bits
+  db 0      ; Base 16-23 bits
+  db 0x9a   ; Access bites
+  db 11001111b ; High 4 bits flags and low 4 bits fflags
+  db 0      ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:   ; DS, SS, ES, FS, GS
+  dw 0xffff ; Segment limit first 0-15 bits
+  dw 0      ; Base first 0-15 bits
+  db 0      ; Base 16-23 bits
+  db 0x92   ; Access bites
+  db 11001111b ; High 4 bits flags and low 4 bits fflags
+  db 0      ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+  dw gdt_end - gdt_start-1
+  dd gdt_start
+
+[BITS 32]
+load32:
+  mov ax, DATA_SEG
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov ebp, 0x00200000
+  mov esp, ebp
   jmp $
-
-error:
-  mov si, error_message
-  call print
-  jmp $
-
-print:
-  mov bx, 0
-
-.loop:
-  lodsb
-  cmp al, 0
-  je .done
-  call print_char
-  jmp .loop
-
-.done:
-  ret
-
-print_char:
-  mov ah, 0eh
-  int 0x10
-  ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer:
