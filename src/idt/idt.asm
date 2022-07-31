fcd209c72d2de39dd3f8edc4e7e08bf5bf495d3c
Fedi Nabli <fedinabli@gmail.com>
2022-07-31 22:18:25 +0100

    Implemented programmable interrupt controller

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -1,6 +1,12 @@
 section .asm
 
+extern int21h_handler
+extern no_interrupt_handler
+
+global int21h
 global idt_load
+global no_interrupt
+
 idt_load:
   push ebp
   mov ebp, esp
@@ -9,3 +15,19 @@ idt_load:
   lidt [ebx]
   pop ebp
   ret
+
+int21h:
+  cli
+  pushad
+  call int21h_handler
+  popad
+  sti
+  iret
+
+no_interrupt:
+  cli
+  pushad
+  call no_interrupt_handler
+  popad
+  sti
+  iret
