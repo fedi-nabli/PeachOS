c835e3f7094338bd2a901cee1e679aa76848aa3a
Fedi Nabli <fedinabli@gmail.com>
2022-08-04 13:15:17 +0100

    Fixed typo

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -12,7 +12,7 @@ struct paging_4gb_chunk* paging_new_4gb(uint8_t flags) {
 
   for (int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; i++) {
     uint32_t* entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-    for (int b = 0; b < PAGING_TOTAL_ENTRIES_PER_TABLE; i++) {
+    for (int b = 0; b < PAGING_TOTAL_ENTRIES_PER_TABLE; b++) {
       entry[b] = (offset + (b * PAGING_PAGE_SIZE)) | flags;
     }
     offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE);
