7f2d881264e12cd6eb1abbef8b8a53af88ed64bd
Fedi Nabli <fedinabli@gmail.com>
2022-08-04 22:38:47 +0100

    Making a path parser

---
+++ src/fs/pparser.h
@@ -0,0 +1,17 @@
+#ifndef PATHPARSER_H
+#define PATHPARSER_H
+
+struct path_root {
+  int driver_no;
+  struct path_part* first;
+};
+
+struct path_part {
+  const char* part;
+  struct path_part* next;
+};
+
+struct path_root* pathparser_parse(const char* path, const char* current_directory_path);
+void pathparser_free(struct path_root* root);
+
+#endif
--- src/kernel.h
+++ src/kernel.h
@@ -2,7 +2,9 @@
 #define KERNEL_H
 
 #define VGA_WIDTH 80
-#define VGA_HEIGHT 20 
+#define VGA_HEIGHT 20
+
+#define PEACHOS_MAX_PATH 108
 
 void kernel_main();
 void print(const char* str);
