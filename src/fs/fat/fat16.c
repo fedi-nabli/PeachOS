5ae84925d8e7d745c7950e169351251754b6d426
Fedi Nabli <fedinabli@gmail.com>
2022-08-07 18:35:32 +0100

    Implementing FAT16 fread function

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -112,10 +112,12 @@ struct fat_private {
 
 int fat16_resolve(struct disk* disk);
 void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode);
+int fat16_read(struct disk* disk, void* descriptor, uint32_t size, uint32_t nmemb, char* out_ptr);
 
 struct filesystem fat16_fs = {
   .resolve = fat16_resolve,
-  .open = fat16_open
+  .open = fat16_open,
+  .read = fat16_read
 };
 
 struct filesystem* fat16_init() {
@@ -386,7 +388,7 @@ static int fat16_read_internal_from_stream(struct disk* disk, struct disk_stream
   int offset_from_cluster = offset % size_of_cluster_bytes;
 
   int starting_sector = fat16_cluster_to_sector(private, cluster_to_use);
-  int starting_pos = (starting_sector * disk->sector_size) * offset_from_cluster;
+  int starting_pos = (starting_sector * disk->sector_size) + offset_from_cluster;
   int total_to_read = total > size_of_cluster_bytes ? size_of_cluster_bytes : total;
   res = diskstreamer_seek(stream, starting_pos);
   if (res != PEACHOS_ALL_OK) {
@@ -526,7 +528,7 @@ struct fat_item* fat16_get_directory_entry(struct disk* disk, struct path_part* 
     current_item = tmp_item;
     next_part = next_part->next;
   }
-  
+
   out:
     return current_item;
 }
@@ -549,4 +551,25 @@ void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode) {
 
   descriptor->pos = 0;
   return descriptor;
+}
+
+int fat16_read(struct disk* disk, void* descriptor, uint32_t size, uint32_t nmemb, char* out_ptr) {
+  int res = 0;
+  struct fat_file_descriptor* fat_desc = descriptor;
+  struct fat_directory_item* item = fat_desc->item->item;
+  int offset = fat_desc->pos;
+  for (uint32_t i = 0; i < nmemb; i++) {
+    res = fat16_read_internal(disk, fat16_get_first_cluster(item), offset, size, out_ptr);
+    if (ISERR(res)) {
+      goto out;
+    }
+
+    out_ptr += size;
+    offset += size;
+  }
+
+  res = nmemb;
+
+  out:
+    return res;
 }
