a188f5a077d9babaf0009b3ac76898877f7e16ea
Fedi Nabli <fedinabli@gmail.com>
2022-08-07 18:11:26 +0100

    Implementing the VFS fread function

--- src/fs/file.h
+++ src/fs/file.h
@@ -2,6 +2,7 @@
 #define FILE_H
 
 #include "pparser.h"
+#include <stdint.h>
 
 typedef unsigned int FILE_SEEK_MODE;
 
@@ -21,12 +22,15 @@ enum {
 
 struct disk;
 typedef void* (*FS_OPEN_FUNCTION) (struct disk* disk, struct path_part* path, FILE_MODE mode);
+typedef int (*FS_READ_FUNCTION) (struct disk* disk, void* private, uint32_t size, uint32_t nmemb, char* out);
+
 typedef int (*FS_RESOLVE_FUNCTION) (struct disk* disk);
 
 struct filesystem {
   // Filesystem should return 0 from resolve if the provided disk is using it's filesystem
   FS_RESOLVE_FUNCTION resolve;
   FS_OPEN_FUNCTION open;
+  FS_READ_FUNCTION read;
 
   char name[20];
 };
@@ -45,6 +49,8 @@ struct file_descriptor {
 
 void fs_init();
 int fopen(const char* filename, const char* mode_str);
+int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd);
+
 void fs_insert_filesystem(struct filesystem* filesystem);
 struct filesystem* fs_resolve(struct disk* disk);
 
--- src/kernel.c
+++ src/kernel.c
@@ -86,7 +86,7 @@ void kernel_main() {
   // Enable the system interrupts
   enable_interrupts();
 
-  int fd = fopen("0:/hello.txt", "r");
+  int fd = fopen("0:/hello2.txt", "r");
   if (fd) {
     print("We opened hello.txt");
   }
