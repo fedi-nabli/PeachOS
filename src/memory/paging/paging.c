ea91f09ca98049506c9aa23a7b9942ba371c3614
Fedi Nabli <fedinabli@gmail.com>
2022-08-11 15:06:44 +0100

    Changing the paging functionality

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -25,9 +25,9 @@ struct paging_4gb_chunk* paging_new_4gb(uint8_t flags) {
   return chunk_4gb;
 }
 
-void paging_switch(uint32_t* directory) {
-  paging_load_directory(directory);
-  current_directory = directory;
+void paging_switch(struct paging_4gb_chunk* directory) {
+  paging_load_directory(directory->directory_entry);
+  current_directory = directory->directory_entry;
 }
 
 void paging_free_4gb(struct paging_4gb_chunk* chunk) {
@@ -72,15 +72,15 @@ void* paging_align_address(void* ptr) {
   return ptr;
 }
 
-int paging_map(uint32_t* directory, void* virt, void* phys, int flags) {
+int paging_map(struct paging_4gb_chunk* directory, void* virt, void* phys, int flags) {
   if (((unsigned int) virt % PAGING_PAGE_SIZE) || ((unsigned int) phys % PAGING_PAGE_SIZE)) {
     return -EINVARG;
   }
 
-  return paging_set(directory, virt, (uint32_t) phys | flags);
+  return paging_set(directory->directory_entry, virt, (uint32_t) phys | flags);
 }
 
-int paging_map_range(uint32_t* directory, void* virt, void* phys, int count, int flags) {
+int paging_map_range(struct paging_4gb_chunk* directory, void* virt, void* phys, int count, int flags) {
   int res = 0;
   for (int i = 0; i < count; i++) {
     res = paging_map(directory, virt, phys, flags);
@@ -93,7 +93,7 @@ int paging_map_range(uint32_t* directory, void* virt, void* phys, int count, int
   return res;
 }
 
-int paging_map_to(uint32_t* directory, void* virt, void* phys, void* phy_end, int flags) {
+int paging_map_to(struct paging_4gb_chunk* directory, void* virt, void* phys, void* phy_end, int flags) {
   int res = 0;
   if((uint32_t) virt % PAGING_PAGE_SIZE) {
     res = -EINVARG;
