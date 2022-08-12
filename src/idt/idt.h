1127abea64e00139d1006b3564676c5d84d867d3
Fedi Nabli <fedinabli@gmail.com>
2022-08-12 11:34:51 +0100

    Creating the ability to create and execute kernel commands

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+struct interrupt_frame;
+typedef void* (*ISR80H_COMMAND) (struct interrupt_frame* frame);
+
 struct idt_desc {
   uint16_t offset_1; // Offset bits 0 - 15
   uint16_t selector; // Selector thats in our GDT
@@ -35,5 +38,6 @@ struct interrupt_frame {
 void idt_init();
 void enable_interrupts();
 void disable_interrupts();
+void isr80h_register_command(int command_id, ISR80H_COMMAND command);
 
 #endif
