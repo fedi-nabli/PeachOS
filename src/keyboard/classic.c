6b61812a5cf5975632607f211dccbecbccb0983f
Fedi Nabli <fedinabli@gmail.com>
2022-08-13 18:08:13 +0100

    Craeting the PS2 port keyboard driver part 1

---
+++ src/keyboard/classic.c
@@ -0,0 +1,48 @@
+#include "classic.h"
+#include "keyboard.h"
+#include "io/io.h"
+#include <stdint.h>
+#include <stddef.h>
+
+int classic_keyboard_init();
+
+static uint8_t keyboard_scan_set_one[] = {
+  0x00, 0x1B, '1', '2', '3', '4', '5',
+  '6', '7', '8', '9', '0', '-', '=',
+  0x08, '\t', 'A', 'A', 'E', 'R', 'T',
+  'Y', 'U', 'I', 'O', 'P', '^', '$',
+  0x0d, 0x00, 'Q', 'S', 'D', 'F', 'G',
+  'H', 'J', 'K', 'L', 'M', '%', '`', 
+  0x00, '<', 'W', 'X', 'C', 'V', 'B',
+  'N', ',', ';', ':', '!', 0x00, '*',
+  0x00, 0x20, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, '7', '8', '9', '-', '4', '5',
+  '6', '+', '1', '2', '3', '0', '.'
+};
+
+struct keyboard classic_keyboard = {
+  .name = {"Classic"},
+  .init = classic_keyboard_init
+};
+
+int classic_keyboard_init() {
+  outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT);
+  return 0;
+}
+
+uint8_t classic_keyboard_scancode_to_char(uint8_t scancode) {
+  size_t size_of_keyboard_set_one = sizeof(keyboard_scan_set_one) / sizeof(uint8_t);
+  if (scancode > size_of_keyboard_set_one) {
+    return 0;
+  }
+
+  char c = keyboard_scan_set_one[scancode];
+  return c;
+}
+
+void classic_keyboard_handle_interrupt() {}
+
+struct keyboard* classic_init() {
+  return &classic_keyboard;
+}
