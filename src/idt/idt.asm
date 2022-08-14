7a99dac85640cd7dfabd59cbbf65062054970368
Fedi Nabli <fedinabli@gmail.com>
2022-08-14 18:53:25 +0100

    Improving our interrupt descriptor table design

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -1,15 +1,15 @@
 section .asm
 
-extern int21h_handler
 extern no_interrupt_handler
 extern isr80h_handler
+extern interrupt_handler
 
-global int21h
 global idt_load
 global no_interrupt
 global enable_interrupts
 global disbale_interrupts
 global isr80h_wrapper
+global interrupt_pointer_table
 
 enable_interrupts:
   sti
@@ -28,17 +28,38 @@ idt_load:
   pop ebp
   ret
 
-int21h:
-  pushad
-  call int21h_handler
-  popad
-  iret
-
 no_interrupt:
   pushad
   call no_interrupt_handler
   popad
   iret
+
+%macro interrupt 1
+  global int%1
+  int%1:
+    ; INTERRUPT FRAME START
+    ; ALREADY PUSHED TO US BY THE PROCESSOR UPON ENTRY TO THIS INTERRUPT
+    ; uint32_t ip
+    ; uint32_t cs
+    ; uint32_t flags
+    ; uint32_t sp
+    ; uint32_t ss
+    ; Pushes the general purpose registers to the stack
+    pushad
+    ; INTERRUPT FARME END
+    push esp
+    push dword %1
+    call interrupt_handler
+    add esp, 8
+    popad
+    iret
+%endmacro
+
+%assign i 0
+%rep 512
+  interrupt i
+%assign i i+1
+%endrep
 
 isr80h_wrapper:
   ; INTERRUPT FRAME START
@@ -70,3 +91,14 @@ isr80h_wrapper:
 section .data
 ; Inside here is stored the return result from isr80h_handler
 tmp_res: dd 0
+
+%macro interrupt_array_entry 1
+  dd int%1
+%endmacro
+
+interrupt_pointer_table:
+%assign i 0
+%rep 512
+  interrupt_array_entry i
+%assign i i+1
+%endrep
--- src/idt/idt.c
+++ src/idt/idt.c
@@ -8,19 +8,19 @@
 struct idt_desc idt_descriptors[PEACHOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+extern void* interrupt_pointer_table[PEACHOS_TOTAL_INTERRUPTS];
+
 static ISR80H_COMMAND isr80h_commands[PEACHOS_MAX_ISR80H_COMMANDS];
 
 extern void idt_load(struct idtr_desc* ptr);
-extern void int21h();
 extern void no_interrupt();
 extern void isr80h_wrapper();
 
-void int21h_handler() {
-  print("Keyboard pressed!\n");
+void no_interrupt_handler() {
   outb(0x20, 0x20);
 }
 
-void no_interrupt_handler() {
+void interrupt_handler(int interrupt, struct interrupt_frame* frame) {
   outb(0x20, 0x20);
 }
 
@@ -43,11 +43,10 @@ void idt_init() {
   idtr_descriptor.base = (uint32_t) idt_descriptors;
 
   for (int i = 0; i < PEACHOS_TOTAL_INTERRUPTS; i++) {
-    idt_set(i, no_interrupt);
+    idt_set(i, interrupt_pointer_table[i]);
   }
 
   idt_set(0, idt_zero);
-  idt_set(0x21, int21h);
   idt_set(0x80, isr80h_wrapper);
 
   // Load the interrupt descriptor table
