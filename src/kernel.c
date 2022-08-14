0147c4f7b4a4080595bae1de4d7a9f0bcc0b24ea
Fedi Nabli <fedinabli@gmail.com>
2022-08-14 21:51:02 +0100

    Creating the PS2 port keyboard driver part 2

--- src/kernel.c
+++ src/kernel.c
@@ -88,10 +88,6 @@ struct gdt_structured gdt_structured[PEACHOS_TOTAL_GDT_SEGMENTS] = {
   {.base = (uint32_t) &tss, .limit = sizeof(tss), .type = 0xE9}    // TSS segment
 };
 
-void pic_timer_callback(struct interrupt_frame* frame) {
-  print("Timer activated!\n");
-}
-
 void kernel_main() {
   terminal_initialize();
 
@@ -136,15 +132,11 @@ void kernel_main() {
   // Initialize all the system keyboards
   keyboard_init();
 
-  idt_register_interrupt_callback(0x20, pic_timer_callback);
-
   struct process* process = 0;
   int res = process_load_switch("0:/blank.bin", &process);
   if (res != PEACHOS_ALL_OK) {
     panic("Failed to load blank.bin\n");
   }
-
-  keyboard_push('A');
 
   task_run_first_ever_task();
 
