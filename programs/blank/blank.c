992274b44b115777fb7e9423098f6c93d2654f14
Fedi Nabli <fedinabli@gmail.com>
2022-08-17 23:38:17 +0100

    Creating an exit command

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -7,10 +7,6 @@ int main(int argc, char** argv) {
   for (int i = 0; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
-
-  char* ptr = (char*) 0x00;
-  *ptr = 0x50;
   
-  while (1) {}
   return 0;
 }
