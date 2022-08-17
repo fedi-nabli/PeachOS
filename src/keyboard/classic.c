53b12a4f3de96eeed21a55f07b4ff92f93556886
Fedi Nabli <fedinabli@gmail.com>
2022-08-17 23:51:01 +0100

    Handling caps lock

--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define CLASSIC_KEYBOARD_CAPSLOCK 0x3A
+
 int classic_keyboard_init();
 
 static uint8_t keyboard_scan_set_one[] = {
@@ -35,6 +37,8 @@ void classic_keyboard_handle_interrupt();
 int classic_keyboard_init() {
   idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, classic_keyboard_handle_interrupt);
 
+  keyboard_set_capslock(&classic_keyboard, KEYBOARD_CAPS_LOCK_OFF);
+
   outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT);
   return 0;
 }
@@ -46,6 +50,12 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode) {
   }
 
   char c = keyboard_scan_set_one[scancode];
+  if (keyboard_get_capslock(&classic_keyboard) == KEYBOARD_CAPS_LOCK_OFF) {
+    if (c >= 'A' && c <= 'Z') {
+      c += 32;
+    }
+  }
+
   return c;
 }
 
@@ -57,6 +67,11 @@ void classic_keyboard_handle_interrupt() {
 
   if (scancode & CLASSIC_KEYBOARD_KEY_RELEASED) {
     return;
+  }
+
+  if (scancode == CLASSIC_KEYBOARD_CAPSLOCK) {
+    KEYBOARD_CAPS_LOCK_STATE old_state = keyboard_get_capslock(&classic_keyboard);
+    keyboard_set_capslock(&classic_keyboard, old_state == KEYBOARD_CAPS_LOCK_ON ? KEYBOARD_CAPS_LOCK_OFF : KEYBOARD_CAPS_LOCK_ON);
   }
 
   uint8_t c = classic_keyboard_scancode_to_char(scancode);
